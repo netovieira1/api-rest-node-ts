b7ce31ae129364878ba50ecd75b423ec17eea904
Neto Vieira <128878577+netovieira1@users.noreply.github.com>
2024-03-19 03:11:57 -0300

    feat: seeds

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,19 @@ import { Knex } from './server/database/knex'
 const startServer = () => {
   server.listen(process.env.PORT || 3333, () => {
     console.log(`App rodando na porta  ${process.env.PORT || 3333}`)
-  })
+ })
 }
 
 if(process.env.IS_LOCALHOST !== 'true') {
-  Knex.migrate.latest()
-  .then(() => {
-    startServer()
-}) 
+  console.log('Rodando migrations')
+
+  Knex.migrate
+  .latest()
+  .then(() => { 
+  Knex.seed.run()
+    .then(() => startServer())
+    .catch(console.log)
+  })
   .catch(console.log)
 } else {
   startServer()
