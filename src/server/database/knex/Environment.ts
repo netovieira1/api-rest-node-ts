a90888dda7d8fdd250c058a34b3ee7d148309661
Neto Vieira <128878577+netovieira1@users.noreply.github.com>
2024-04-11 01:03:06 -0300

    feat: add environment setings

--- src/server/database/knex/Environment.ts
+++ src/server/database/knex/Environment.ts
@@ -28,21 +28,21 @@ export const test: Knex.Config = {
 
 export const production: Knex.Config = {
   ...development,
-  // client: 'sqlite3',
-  // migrations: {
-  //   directory: path.resolve(__dirname, '..', 'migrations')
-  // },
-  // seeds: {
-  //   directory: path.resolve(__dirname, '..', 'seeds')
-  // },
-  // connection: {
-  //   host: process.env.DATABASE_HOST,
-  //    user: process.env.DATABASE_USER,
-  //    database: process.env.DATABASE_NAME,
-  //    password: process.env.DATABASE_PASSWORD,
-  //    port: Number(process.env.DATABASE_PORT || 5432),
-  //    ssl: { rejectUnauthorized: false },
-  // },
+  client: 'sqlite3',
+  migrations: {
+    directory: path.resolve(__dirname, '..', 'migrations')
+  },
+  seeds: {
+    directory: path.resolve(__dirname, '..', 'seeds')
+  },
+  connection: {
+    host: process.env.DATABASE_HOST,
+     user: process.env.DATABASE_USER,
+     database: process.env.DATABASE_NAME,
+     password: process.env.DATABASE_PASSWORD,
+     port: Number(process.env.DATABASE_PORT || 5432),
+     ssl: { rejectUnauthorized: false },
+  },
 }
 
 
